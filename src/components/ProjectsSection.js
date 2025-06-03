bc5896f6570e9cae28a85d69798fd66dab825396
Yeinier <yeinierv@gmail.com>
2025-06-03 17:09:44 -0400

    update projects in ProjectsSection.js

--- src/components/ProjectsSection.js
+++ src/components/ProjectsSection.js
@@ -9,25 +9,28 @@ const projects = [
     description:
       "Full-featured flight search engine using the Amadeus API. Users can search, filter, and compare flight options in real time with a modern responsive UI. ",
     getImageSrc: () => require("../images/flight.jpg"),
-    web:"https://flightfinder2025.netlify.app/",
+    web: "https://flightfinder2025.netlify.app/",
   },
   {
     title: "Movie Explorer",
     description:
       "A React-based application that consumes The Movie Database (TMDb) API to display trending movies dynamically. It features a real-time search bar with instant suggestions as you type, and implements infinite scroll to automatically load more movies as the user scrolls down.",
     getImageSrc: () => require("../images/movie.jpg"),
+    web: "https://movies-yeinier.netlify.app/",
   },
   {
-    title: "Photo Gallery",
+    title: "Little Lemon Restaurant Website",
     description:
-      "A One-stop shop for photographers to share and monetize their photos, allowing them to have a second source of income",
-    getImageSrc: () => require("../images/photo3.jpg"),
+      "A responsive React-based restaurant website featuring a reservation system with realistic availability simulation. Users can select the date, time, and number of guests, and the app determines table availability dynamically, mimicking a real API response. The interface adapts to both desktop and mobile views, and includes sections for the weekly specials, testimonials, and general information.",
+    getImageSrc: () => require("../images/restaurant.jpg"),
+    web: "https://little-lemmon-yeinier.netlify.app/",
   },
   {
-    title: "Event planner",
+    title: "Taskify - Coming Soon",
     description:
-      "A mobile application for leisure seekers to discover unique events and activities in their city with a few taps",
-    getImageSrc: () => require("../images/photo4.jpg"),
+      /*"A task-tracking app built with React and Firebase. Users can register, log in, and manage their personal task list with full CRUD operations. The app includes protected routes, mobile-friendly design, and real-time state updates.",*/
+      "This project is currently under development. It will include user authentication, protected routes, and full CRUD functionality using Firebase.",
+    getImageSrc: () => require("../images/login.jpg"),
   },
 ];
 
